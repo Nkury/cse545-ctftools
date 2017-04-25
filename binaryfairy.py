37b833d800bbfc746ee6c494fea4638def234852
SPARCpenguin <sean.gatenby@asu.edu>
2017-04-25 13:18:16 -0700

    Added import to angr.io and started messing with angr

--- binaryfairy.py
+++ binaryfairy.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python2
 
+import os
 import sys
+import angr
 
 # Global "bad" function array
 bad_functions = ["strcpy", "strcat", "gets", "puts"]
@@ -14,5 +16,17 @@ def usage():
 if(len(sys.argv) != 2):
     usage()
     exit(-1)
+
+print("Parsing " + sys.argv[1])
+
+# Open file for parsing
+#load_options = {'auto_load_libs': False}
+proj = angr.Project(sys.argv[1])
+
+print("Binary Name: " + proj.filename)
+print("Binary Arch: " + str(proj.arch))
+print("Binary Entry: " + str(proj.entry))
+
+cfg = proj.analyses.CFG()
 
 print("~Hey, Listen!!~")
