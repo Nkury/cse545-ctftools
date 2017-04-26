527dbdbb20f139c7dece332f582c30e42b2b7f28
SPARCpenguin <sean.gatenby@asu.edu>
2017-04-26 11:43:56 -0700

    Got angr CFG calls working

--- binaryfairy.py
+++ binaryfairy.py
@@ -20,13 +20,13 @@ if(len(sys.argv) != 2):
 print("Parsing " + sys.argv[1])
 
 # Open file for parsing
-#load_options = {'auto_load_libs': False}
-proj = angr.Project(sys.argv[1])
+proj = angr.Project(sys.argv[1], load_options={'auto_load_libs': False})
 
 print("Binary Name: " + proj.filename)
 print("Binary Arch: " + str(proj.arch))
 print("Binary Entry: " + str(proj.entry))
 
 cfg = proj.analyses.CFG()
+print(dict(proj.kb.functions))
 
 print("~Hey, Listen!!~")
