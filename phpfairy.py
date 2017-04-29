b17b69017c6dcbacaa1c80370f539d3ec9fd3660
jasonsymmes <jsymmes@asu.edu>
2017-04-28 19:37:20 -0700

    Now with command injection

--- phpfairy.py
+++ phpfairy.py
@@ -13,7 +13,7 @@ else:
 if(len(sys.argv) != 2 and not debug):
     usage()
     exit(-1)
-
+CIA = ('grep','system','eval','popen','include','require')
 req = ("GET[", "POST[")
 lineNum = 0
 UCvars = []
@@ -22,12 +22,16 @@ for line in open(file):
         start = line.find('\$') + 1
         end = line.find(' ', start)
         UCvars.append(line[start:end])
-    if any(s in line for s in UCvars) and 'mysql_query' in line:
+    if (any(s in line for s in UCvars) or any(s in line for s in req)) and 'mysql_query' in line:
         warn = "Hey! Listen! Possible SQL injection at line " + str(lineNum) + ":"
         print(warn)
         print(line)
-    if any(s in line for s in UCvars) and 'echo' in line:
+    if (any(s in line for s in UCvars) or any(s in line for s in req)) and ('echo' in line or 'put_file_contents' in line) and ('htmlspecialchars' and 'htmlentities' not in line) :
         warn = "Hey! Listen! Possible XSS at line " + str(lineNum) + ":"
+        print(warn)
+        print(line)
+    if (any(s in line for s in UCvars) or any(s in line for s in req)) and any(s in line for s in CIA):
+        warn = "Hey! Listen! Possible OS command injection at line " + str(lineNum) + ":"
         print(warn)
         print(line)
     lineNum = lineNum + 1
