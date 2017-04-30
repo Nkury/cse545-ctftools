8b33b642df337d5f631dba16f4fe1a9c3c622bdc
SPARCpenguin <sean.gatenby@asu.edu>
2017-04-30 16:04:22 -0700

    Working on finding buffers

--- binaryfairy.py
+++ binaryfairy.py
@@ -4,6 +4,8 @@ import os
 import sys
 import angr
 import re
+import archinfo
+
 #import overflow_detection
 
 # Global "bad" function array
@@ -30,15 +32,17 @@ def usage():
     print("Usage: " + __file__ + " <path to binary file>")
     print("\tExample: " + __file__ + " ./test.bin")
 
-# Generate CFG for the binary and return it
-def generateCFG(filename):
+def openProj(filename):
     # Open file for parsing
     proj = angr.Project(filename, load_options={'auto_load_libs': False})
 
     print("Binary Name: " + proj.filename)
     print("Binary Arch: " + str(proj.arch))
     print("Binary Entry: " + str(proj.entry))
+    return proj
 
+# Generate CFG for the binary and return it
+def generateCFG(proj):
     # Generate control flow graph for binary
     cfg = proj.analyses.CFG()
     return cfg
@@ -51,7 +55,6 @@ def locateVulnerableFunctions(cfg):
     vulnFuncIndex = 0
     vulnFuncName = ""
     vulnFuncAddr = 0x0
-    # vFunc = vulnFunc()
 
     # Search CFG for calls to unsafe functions
     for unsafeFunc in unsafe_functions:
@@ -98,19 +101,35 @@ def locateVulnerableFunctions(cfg):
 
                                 vulnFuncs.append(vFunc)
 
-    print("\nPrinting Current vulnFunc Array")
-    for i, f in enumerate(vulnFuncs):
-        print("vFunc[" + str(i) + "].name: " + f.name)
-        print("vFunc[" + str(i) + "].addr: 0x" + str(format(f.addr, 'x')))
-        for j, u in enumerate(f.unsafeFuncList):
-            print("vFunc[" + str(i) + "].unsafeFuncList[" + str(j) + "]: " + u)
-            print("vFunc[" + str(i) + "].unsafeFuncAddrList[" + str(j) + "]: 0x" + str(format(f.unsafeFuncAddrList[j], 'x')))
-    print("done\n")
-
     return vulnFuncs
 
-def disssembleBinary(filename):
-    objdump = os.popen('objdump -d ' + sys.argv[1]).read()
+def locateBuffers(vulnFunc, arch):
+    # Chop up the disassembly so we can parse it line by line
+    disasm = vulnFunc.disassembly.splitlines(True)
+
+    # Figure out what ebp will be called in the disassembly
+    if arch.name == archinfo.ArchAMD64.name:
+        ebp = "%rbp"
+    elif arch.name == archinfo.ArchX86.name:
+        ebp = "%ebp"
+    else:
+        sys.exit("Unsupported architecture")
+
+    # Loop through the disassembly code and search for reference to %ebp-value
+    # If we see %ebp-value before a call to an unsafe function, there may be a
+    # buffer overflow vulnerability
+    for lines in disasm:
+        match = re.search('^\s*(\w+?):\s*?\w*?\s*(.+?)$', lines)
+
+        if match:
+            print(match.group(1) + " " + match.group(2))
+        else:
+            print("Match failed:\n" + lines)
+
+
+
+def disassembleBinary(filename):
+    objdump = os.popen('objdump -dC --no-show-raw-insn ' + sys.argv[1]).read()
     return objdump
 
 def parseDisassembly(objdump, vulnFuncs):
@@ -126,8 +145,10 @@ if(len(sys.argv) != 2):
     usage()
     exit(-1)
 
+proj = openProj(sys.argv[1])
+
 # Generate CFG
-cfg = generateCFG(sys.argv[1])
+cfg = generateCFG(proj)
 
 # Locate all functions that call vulnerable function
 # vulnFuncs is a dictionary with key = <name> and value = <entry point>
@@ -135,17 +156,20 @@ vulnFuncs = locateVulnerableFunctions(cfg)
 
 # Disassemble binary
 # objdump is the output of running objdump
-objdump = disssembleBinary(sys.argv[1])
+objdump = disassembleBinary(sys.argv[1])
 
 # Chop up disassembly to only include vulnerable functions
 # dissFuncs is a dictionary with key = <name> and value = <objdump for function>
 dissFuncs = parseDisassembly(objdump, vulnFuncs)
 
 for vFunc in vulnFuncs:
-    print("\n~Hey, Listen!! " + vFunc.name + "(" + str(format(vFunc.addr, 'x')) + ") calls:\n")
+    print("\n~Hey, Listen!!~ " + vFunc.name + "(" + str(format(vFunc.addr, 'x')) + ") calls:")
 
     for index, func in enumerate(vFunc.unsafeFuncList):
         print(str(func) + " at address 0x" + str(format(vFunc.unsafeFuncAddrList[index], 'x')))
 
-    print(vFunc.disassembly)
+    # print("Disassembly of " + vFunc.name + ":")
+    # print(vFunc.disassembly)
+
+    locateBuffers(vFunc, proj.arch)
 
